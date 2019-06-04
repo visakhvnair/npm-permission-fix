2976bf55b39e49b1b650364c5fd26e103f905ebe
VISAKH V NAIR <16491716+visakhvnair@users.noreply.github.com>
2019-06-04 11:48:39 +0530

    updated script to bash

--- npm-global-installtion-permission-fix.sh
+++ npm-global-installtion-permission-fix.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 mkdir ~/.npm-global
 if [ $? -eq 0 ]
 then
 	npm config set prefix '~/.npm-global'
 	if [ $? -eq 0 ]
 	then
-		echo  "#Added by user `whoami` \n export PATH=~/.npm-global/bin:\$PATH" >> ~/.profile
+		echo -e "#Added by user `whoami` \n export PATH=~/.npm-global/bin:\$PATH" >> ~/.profile
 		if [ $? -eq 0 ]
 		then
 
@@ -13,26 +14,26 @@ then
 			then
 				echo "Successfully Fixed"
 			else
-				echo  "Cannot source ~/.profile manually run  \e[40m\e[92msource ~/.profile\e[0m"
+				echo -e "Cannot source ~/.profile manually run  \e[40m\e[92msource ~/.profile\e[0m"
 				exit
 			fi
 		else
-			echo  "Error: Manually run  \n\e[40m\e[92mecho -e \"#Added by user `whoami` \\\n export PATH=~/.npm-global/bin:\$PATH\" >> ~/.profile\e[0m"
-			echo "\e[40m\e[92msource ~/.profile\e[0m"
+			echo -e "Error: Manually run  \n\e[40m\e[92mecho -e \"#Added by user `whoami` \\\n export PATH=~/.npm-global/bin:\$PATH\" >> ~/.profile\e[0m"
+			echo -e "\e[40m\e[92msource ~/.profile\e[0m"
 			exit
 		fi 
 	else
 		
-		echo "Error: Manually run the following \n \e[40m\e[92m\nnpm config set prefix '~/.npm-global'\e[0m"
-		echo "\e[40m\e[92mecho -e \"#Added by user `whoami` \\\nexport PATH=~/.npm-global/bin:\$PATH\" >> ~/.profile\e[0m"
-		echo "\e[40m\e[92msource ~/.profile\e[0m"
+		echo -e "Error: Manually run the following \n \e[40m\e[92m\nnpm config set prefix '~/.npm-global'\e[0m"
+		echo -e "\e[40m\e[92mecho -e \"#Added by user `whoami` \\\nexport PATH=~/.npm-global/bin:\$PATH\" >> ~/.profile\e[0m"
+		echo -e "\e[40m\e[92msource ~/.profile\e[0m"
 		exit
 
 
 	fi 
 else
-	echo  "\e[31m\e[40m#########################################";
-	echo  "Script Execution faild";
-	echo  "#########################################\e[0m";
+	echo -e "\e[31m\e[40m#########################################";
+	echo -e "Script Execution faild";
+	echo -e "#########################################\e[0m";
 	exit
 fi 
